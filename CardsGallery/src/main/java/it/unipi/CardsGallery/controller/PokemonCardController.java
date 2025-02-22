cfaff764981fe3e7af2dfee01a95aef699b7f25d
yughias <sasa.laporta@gmail.com>
2025-02-22 16:38:34 +0100

    all endpoints lowercase

--- CardsGallery/src/main/java/it/unipi/CardsGallery/controller/PokemonCardController.java
+++ CardsGallery/src/main/java/it/unipi/CardsGallery/controller/PokemonCardController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/Pokemon")
+@RequestMapping("/pokemon")
 public class PokemonCardController {
 
     @Autowired
--- CardsGallery/src/main/java/it/unipi/CardsGallery/controller/YugiohCardController.java
+++ CardsGallery/src/main/java/it/unipi/CardsGallery/controller/YugiohCardController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/Yugioh")
+@RequestMapping("/yugioh")
 public class YugiohCardController {
 
     @Autowired
